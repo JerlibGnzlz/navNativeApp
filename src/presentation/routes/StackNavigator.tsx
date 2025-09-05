0d8098639f7d090665e7466af4f03bb6c039cf5a
jerlibgnzlz <jerlibgnzlz@gmail.com>
2025-09-04 23:39:57 -0300

    flatlist

--- src/presentation/routes/StackNavigator.tsx
+++ src/presentation/routes/StackNavigator.tsx
@@ -2,15 +2,28 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { HomeScreen } from '../screens/home/HomeScreen';
 import { AboutScreen } from '../screens/about/AboutScreen';
 import { ProductsScreen } from '../screens/products/ProductsScreen';
+import { SettingScreen } from '../screens/setting/SettingScreen';
 
 const Stack = createStackNavigator();
 
 export const StackNavigator = () => {
     return (
-        <Stack.Navigator>
+        <Stack.Navigator screenOptions={{
+            headerShown: true,
+            headerTitle: 'Nav App',
+            headerStyle: {
+                backgroundColor: 'green',
+                shadowColor: 'transparent',
+                elevation: 0
+            },
+            headerTintColor: 'white',
+            headerTitleAlign: 'center'
+
+        }}>
             <Stack.Screen name="Home" component={HomeScreen} />
             <Stack.Screen name="About" component={AboutScreen} />
             <Stack.Screen name="Products" component={ProductsScreen} />
+            <Stack.Screen name="Setting" component={SettingScreen} />
         </Stack.Navigator>
     );
 }
--- src/presentation/screens/products/ProductsScreen.tsx
+++ src/presentation/screens/products/ProductsScreen.tsx
@@ -1,10 +1,55 @@
-import { View, Text } from 'react-native'
+import { View, Text, FlatList, TouchableOpacity } from 'react-native'
+import { globalStyles } from '../../themes/themes';
+import { useNavigation } from '@react-navigation/native';
+import { Button } from '@react-navigation/elements';
+
+
 
 export const ProductsScreen = () => {
+
+    const navigation = useNavigation();
+
+
+    const products = [
+        {
+            id: 1,
+            name: 'Producto 1'
+        },
+        {
+            id: 2,
+            name: 'Producto 2'
+        },
+        {
+            id: 3,
+            name: 'Producto 3'
+        },
+        {
+            id: 4,
+            name: 'Producto 4'
+        },
+    ]
+
+
     return (
-        // eslint-disable-next-line react-native/no-inline-styles
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Productos Screen</Text>
+        <View style={globalStyles.container}>
+            <Text >en el Home Screen</Text>
+            <FlatList
+                data={products}
+                renderItem={({ item }) => (
+                    <TouchableOpacity
+                        style={globalStyles.primaryButton}
+                        onPress={() => navigation.navigate('About' as never)}
+                    >
+                        <Text>{item.name}</Text>
+                    </TouchableOpacity>
+                )}
+            />
+
+            <Button
+                style={globalStyles.primaryButton}
+                onPress={() => navigation.navigate('Setting' as never)}>
+                Ajustes
+            </Button>
         </View>
     )
 }
